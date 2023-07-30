227ac7dd7b9e960ad17002c5c092e2abe7d514a6
Caio Damprelli <caiodamprelli@gmail.com>
2023-07-30 19:23:50 -0300

    Utilizando PathParams

---
+++ 05_JavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/com/example/demo/math/SimpleMath.java
@@ -0,0 +1,42 @@
+package com.example.demo.math;
+
+public class SimpleMath {
+
+	public Double sum(Double numberOne,Double numberTwo) {
+		
+		return (numberOne) + (numberTwo);
+		
+	}
+	
+	public Double sub(Double numberOne, Double numberTwo){
+		
+		return (numberOne) - (numberTwo);
+		
+	}
+	
+	public Double multi(Double numberOne,Double numberTwo){
+		
+		return (numberOne) * (numberTwo);
+		
+	}
+	
+	public Double div(Double numberOne, Double numberTwo){
+		
+		return (numberOne) / (numberTwo);
+		
+	}
+	
+	public Double media(Double numberOne, Double numberTwo){
+		
+		return ((numberOne) + (numberTwo)) / 2;
+		
+	}
+	
+	public Double squad(Double numberOne, Double numberTwo){
+		
+		return Math.sqrt((numberOne) + (numberTwo));
+		
+	}
+
+
+}
